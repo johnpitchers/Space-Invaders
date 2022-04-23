0fa35a4d995dd5989b94d85c14ea378760a98659
John Pitchers <john@viperfish.com.au>
2022-04-23 19:36:29 +0800

    Code cleanup and tweaks.

---
+++ script.js
@@ -0,0 +1,6 @@
+// Vite doesn't use a source directory!!!!!
+// I can't get used to haveing all my code
+// files in the root with node_modules and
+// package and config files.
+// But this works well enough.
+import "./src/main";
